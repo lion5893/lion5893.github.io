6d3ce895c30773defaa9381f9c863e4b72e75f23
lion5893 <lion5893@gmail.com>
2016-08-11 10:58:09 +0700

    form_signup

--- form_signup/js/app.js
+++ form_signup/js/app.js
@@ -3,17 +3,29 @@ $(document).ready(function(){
 	var uri_dec = decodeURIComponent(uri);
 	var uri_cut = uri_dec.slice(1);
 	var uri_arr = uri_cut.split("&");
-	//var name = uri_arr[0];
-	// Lấy tên
 	
+	// Cắt và lấy ra các thông tin người dùng đã nhập
+	var arr_rs = [];
 	for (i=0; i<uri_arr.length; i++){
 		var name=uri_arr[i];
 		var name_cut = name.split("=");
-		$('.rs_name').text(name_cut[1]);
+		var str = name_cut[1];
+		var str_cut = str.split('+').join(' ');
+		arr_rs.push(str_cut);
 
 	}
-	
-	//var name_cut = name.split("=")
-	//$('.rs_name').text(name_cut[1]);
+	// In ra kết quả
+	$('.rs_name').text(arr_rs[0]);
+	$('.rs_pass').text(arr_rs[1]);
+	$('.rs_born').text(arr_rs[2]);
+	$('.rs_gender').text(arr_rs[3]);
+	$('.rs_email').text(arr_rs[4]);
+	$('.rs_address').text(arr_rs[5]);
+	$('.rs_phone').text(arr_rs[6]);
 
 })
+// var content = '';
+// $.each(uri_arr, function(key, value){
+// content+= '<p>' + value + '</p>';
+// })
+// $('.rs_name').html(content);
