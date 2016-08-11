9767016af59775e40ba2561bb2af402ff59b7904
lion5893 <lion5893@gmail.com>
2016-08-11 08:37:57 +0700

    update

--- form_signup/js/app.js
+++ form_signup/js/app.js
@@ -3,5 +3,17 @@ $(document).ready(function(){
 	var uri_dec = decodeURIComponent(uri);
 	var uri_cut = uri_dec.slice(1);
 	var uri_arr = uri_cut.split("&");
+	//var name = uri_arr[0];
+	// Lấy tên
+	
+	for (i=0; i<uri_arr.length; i++){
+		var name=uri_arr[i];
+		var name_cut = name.split("=");
+		$('.rs_name').text(name_cut[1]);
+
+	}
+	
+	//var name_cut = name.split("=")
+	//$('.rs_name').text(name_cut[1]);
 
 })
