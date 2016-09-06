a19b81e5943f869c19907f1da690704a4429dfc8
lion5893 <lion5893@gmail.com>
2016-09-06 17:33:31 +0700

    shopphuot

--- shopphuot/js/main.js
+++ shopphuot/js/main.js
@@ -1,12 +1,12 @@
-$(function () {
-    $(".productinfo").slice(0, 4).show();
+$(document).ready(function(){
+    $(".new_items .productinfo").slice(0, 4).show();
     $(".loadmore").on('click', function (e) {
         e.preventDefault();
-        $(".productinfo:hidden").slice(0, 2).slideDown();
+        $(".new_items .productinfo:hidden").slice(0, 4).slideDown();
         $('html,body').animate({
             scrollTop: $(this).offset().top
         }, 1000);
-        if($(".productinfo:hidden").length == 0 ){
+        if($(".new_items .productinfo:hidden").length == 0 ){
             $(".loadmore").hide();
         }
     });
