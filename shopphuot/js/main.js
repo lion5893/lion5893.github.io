4e9d447e7201c339541796ebbe0b3edc981d2a8a
lion5893 <lion5893@gmail.com>
2017-07-12 09:16:10 +0700

    Update main.js

--- shopphuot/js/main.js
+++ shopphuot/js/main.js
@@ -1,18 +1,18 @@
 $(document).ready(function(){
     // Load more item
     $(".new_items .productinfo").slice(0, 8).show(); //Hiển thị ra 8 sản phẩm ban đầu
-    $(".loadmore").on('click', function (e) {
-        e.preventDefault();
-        $(".new_items .productinfo:hidden").slice(0, 4).slideDown(); //Hiển thị thêm 4 sản phẩm khi click loadmore
-        // thanh cuộn trượt xuống khi load thêm sản phẩm
-        $('html,body').animate({
-            scrollTop: $(this).offset().top
-        }, 1000);
-        // tắt nút load more khi load hết sản phẩm
-        if($(".new_items .productinfo:hidden").length == 0 ){
-            $(".loadmore").hide();
-        }
-    }); 
+//     $(".loadmore").on('click', function (e) {
+//         e.preventDefault();
+//         $(".new_items .productinfo:hidden").slice(0, 4).slideDown(); //Hiển thị thêm 4 sản phẩm khi click loadmore
+//         // thanh cuộn trượt xuống khi load thêm sản phẩm
+//         $('html,body').animate({
+//             scrollTop: $(this).offset().top
+//         }, 1000);
+//         // tắt nút load more khi load hết sản phẩm
+//         if($(".new_items .productinfo:hidden").length == 0 ){
+//             $(".loadmore").hide();
+//         }
+//     }); 
     // Hide Modal menu mobile
     $(".register-link").click(function(){
        $('#myModal').modal('hide');
